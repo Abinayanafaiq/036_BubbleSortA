5e64c2f7bd3073e325c0608302273e5accd8b283
Abinayanafaiq <abinayanafaiq550@gmail.com>
2026-03-05 15:29:06 +0700

    for

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -12,5 +12,19 @@ void input() {
     while(true){
         cout << "Masukan banyaknya elemnt (max 20): ";
         cin >> n;
+        if (n <= 20) 
+            break;
+        else
+        {
+            cout << "Maksimal banyaknya elemen adalah 20";
+        }
+    }
+
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan elemen array" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
     }
 }
