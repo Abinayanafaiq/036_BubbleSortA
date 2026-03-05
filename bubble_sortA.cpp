1b68001300f8e785dc5f8d58b1cb7b77fd64f16b
Abinayanafaiq <abinayanafaiq550@gmail.com>
2026-03-05 15:46:58 +0700

    void display

--- bubble_sortA.cpp
+++ bubble_sortA.cpp
@@ -51,3 +51,22 @@ void bubbleSortArray() {
 
     } while (pass <= n - 1); 
 }
+
+void display(){
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Elemen array yang sudah tersusun: " << endl;
+    cout << "====================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << " ";
+        if (j < n - 1) {
+            cout << "-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass =" << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
